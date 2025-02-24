77df2f42350164644d55b5cc0fe70b8cae413de8
A02227129 <a02227129@dpapsb-161390.aggies.usu.edu>
2025-02-24 12:22:58 -0700

    new fix

--- R/aleCI_bag_train.R
+++ R/aleCI_bag_train.R
@@ -6,7 +6,7 @@ bag_train <- function(train.func, var.data, nmodels=20, my.env=environment()){
 		df_long = eval(parse(text=var.data))[sample(nrow(eval(parse(text=var.data))), (nrow(eval(parse(text=var.data)))*(nmodels-1)), replace=TRUE), ]
 		rownames(df_long) <- NULL
 		n = 1
-	        for(i in seq(from=1, to=nrow(df_long)-1, by=(nrow(df_long)/nmodels))){
+	        for(i in seq(from=1, to=nrow(df_long)-1, by=(nrow(df_long)/(nmodels-1)))){
 		        n = n + 1
 		        bag.data <- df_long[i:(i+(nrow(df_long)/nmodels-1)),]
 		        bag.func <- sub(var.data, "bag.data", train.func)
