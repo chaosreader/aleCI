28a34621b992c5f8e2c215d476b3bb5281658639
A02227129 <a02227129@dpapsb-161390.aggies.usu.edu>
2025-02-24 14:32:02 -0700

    need list in rug

--- R/aleCI_plot.R
+++ R/aleCI_plot.R
@@ -1,5 +1,11 @@
 aleCI_plot <- function(X, X.models, pred.fun, mid.ci.fun, low.ci.fun, high.ci.fun, J, K=40, NA.plot = TRUE)
     {
     ab <- aleCI(X, X.models, pred.fun, mid.ci.fun, low.ci.fun, high.ci.fun, J, K, NA.plot)
+    if (class(X) == "data.frame")
+    {
+        XX <- list()
+        XX[[1]] <- X
+        X <- XX
+    }
     aleCI_make_plot(ab, X, J)
     }
