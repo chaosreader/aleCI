ebf990118a64629a260cd1461577755879adab6a
A02227129 <a02227129@dpapsb-161390.aggies.usu.edu>
2025-02-24 11:32:53 -0700

    small fix

--- R/aleCI_bag_train.R
+++ R/aleCI_bag_train.R
@@ -2,14 +2,14 @@ bag_train <- function(train.func, var.data, nmodels=20, my.env=environment()){
     model <- eval(parse(text=train.func))
     df_models = list(type=class(model))
     df_models[[1]] <- model
-    df_long = eval(parse(text=var.data))[sample(nrow(eval(parse(text=var.data))), (nrow(eval(parse(text=var.data)))*nmodels), replace=TRUE), ]
+    df_long = eval(parse(text=var.data))[sample(nrow(eval(parse(text=var.data))), (nrow(eval(parse(text=var.data)))*(nmodels-1)), replace=TRUE), ]
     rownames(df_long) <- NULL
     n = 1
     for(i in seq(from=1, to=nrow(df_long)-1, by=(nrow(df_long)/nmodels))){
+      print("entered loop")
       n = n + 1
       bag.data <- df_long[i:(i+(nrow(df_long)/nmodels-1)),]
       bag.func <- sub(var.data, "bag.data", train.func)
-      print(bag.func)
       model <- eval(parse(text=bag.func))
       df_models[[n]] <- model
         }
